92c1e97448083e5b542e6a75f29a280fe79e30c6
rodrigo <albaricaldirodrigofernando@gmail.com>
2026-09-01 11:20:48 -0400

    Clase ciclos

---
+++ CALCULADORA/Clase_ciclos/Primer_While.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace CALCULADORA.Clase_ciclos
+{
+    public class Primer_While
+    {
+        public static void Main()
+        {
+
+            int num = 0;
+            Console.WriteLine("El numero es;" + num);
+            while (num <= 5)
+            {
+                Console.WriteLine("El numero se actualizo:" + num );
+                num++;
+            }
+        }
+    }
+}
---
+++ CALCULADORA/Clase_ciclos/doWhileif.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CALCULADORA.Clase_ciclos
+{
+    internal class doWhileif
+    {
+    }
+}
